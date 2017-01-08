c5abe5404d1f8c1edb4b62c489797869530b55c4
Ippei Nawate <saku.ni63@gmail.com>
2017-01-09 08:48:35 +0900

    Prepare

--- src/main/java/com/sakuna63/Main.java
+++ src/main/java/com/sakuna63/Main.java
@@ -1,7 +1,51 @@
 package com.sakuna63;
 
+import io.reactivex.Observer;
+import io.reactivex.SingleObserver;
+import io.reactivex.disposables.Disposable;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
+
+    private static class LogObserver<T> implements Observer<T> {
+
+        @Override
+        public void onSubscribe(Disposable d) {
+            System.out.println("onSubscribe() called with: " + "d = [" + d + "]");
+        }
+
+        @Override
+        public void onNext(T t) {
+            System.out.println("onNext() called with: " + "t = [" + t + "]");
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            System.out.println("onError() called with: " + "e = [" + e + "]");
+        }
+
+        @Override
+        public void onComplete() {
+            System.out.println("onComplete() called with: " + "");
+        }
+    }
+
+    private static class LogSingleObserver<T> implements SingleObserver<T> {
+        @Override
+        public void onSubscribe(Disposable d) {
+            System.out.println("onSubscribe() called with: " + "d = [" + d + "]");
+        }
+
+        @Override
+        public void onSuccess(T t) {
+            System.out.println("onSuccess() called with: " + "t = [" + t + "]");
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            System.out.println("onError() called with: " + "e = [" + e + "]");
+        }
     }
 }
---
+++ src/main/java/com/sakuna63/Utils.java
@@ -0,0 +1,7 @@
+package com.sakuna63;
+
+public final class Utils {
+    public static void p(Object msg) {
+        System.out.println(String.valueOf(msg));
+    }
+}
